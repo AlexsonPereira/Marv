e95d8095342da1976c7acfd775cf93e7f913de2c
Alexson Pereira <alexspnpe@gmail.com>
2023-04-24 13:13:56 -0300

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['*.{html,js}'],
   theme: {
     screen: {
       '2xlMax': { max: '1535px' },
