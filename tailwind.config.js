b4a0a0559b2ce07cafdebe90e7d61ce342ae2102
Alexson Pereira <alexspnpe@gmail.com>
2023-04-24 10:03:26 -0300

    add

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    screen: {
+      '2xlMax': { max: '1535px' },
+
+      xlMax: { max: '1279px' },
+
+      lgMax: { max: '1023px' },
+
+      mdMax: { max: '767px' },
+
+      smMax: { max: '639px' }
+    },
+    extend: {
+      backgroundImage: {
+        'main-homepage': "url('/src/assets/img1.jpeg')"
+      }
+    }
+  },
+  plugins: []
+}
